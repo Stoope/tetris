13092a96b448f952ebec4707b948f916041dc5df
Stepan Kurennykh <stoope@outlook.com>
2019-05-19 00:49:06 +0300

    add we_alloc

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -1,5 +1,9 @@
 use wasm_bindgen::prelude::*;
 
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
 extern "C" {
     pub fn alert(s: &str);
@@ -8,4 +12,12 @@ extern "C" {
 #[wasm_bindgen]
 pub fn run(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
 }
