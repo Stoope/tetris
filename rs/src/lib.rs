7d249ac077b984282861419c5740d38e7e0713da
Stepan Kurennykh <stoope@outlook.com>
2019-05-26 20:23:38 +0300

    data struct

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -9,14 +9,26 @@ fn set_panic_hook() {
     console_error_panic_hook::set_once();
 }
 
+
 #[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    None = 0,
+    IBlock = 1,
+    JBlock = 2,
+    LBlock = 3,
+    OBlock = 4,
+    SBlock = 5,
+    TBlock = 6,
+    ZBlock = 7,
 }
 
 #[wasm_bindgen]
-pub fn run(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub struct Board {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
 }
 
 #[cfg(test)]
